eab488297dab3352761995c92d0cb10e3d84e905
Arthur Van de Venne <arthur.vandevenne@gmail.com>
2023-11-14 17:02:13 +0000

    fzbhsbqhsqb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -6,4 +7,12 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+#   en
+Article.destroy_all
+10.times do
+  articles = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+  )
+  articles.save!
+end
